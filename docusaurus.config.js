d2d22c5804587dc29f2fe177fa733b9556812dc9
chashu <allen.boyce@gmail.com>
2025-01-04 09:49:28 -0800

    baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
+  title: "My Sitex",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
@@ -18,7 +18,7 @@ const config = {
   url: "https://cagrx.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/todo-doc/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
